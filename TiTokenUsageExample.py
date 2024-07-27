77c9b28de23981d845ebf738e3851c3eae1e096f
mazerlodge <mazerlodge@hotmail.com>
2024-07-27 09:44:56 -0500

    Retested tiktoken, tried out various word->token encodings

--- TiTokenUsageExample.py
+++ TiTokenUsageExample.py
@@ -2,15 +2,22 @@
 from importlib.metadata import version
 import tiktoken
 
+# QNote: This is important, getting gpt2 provided tokens.
 tokenizer = tiktoken.get_encoding("gpt2")
 
 text = ("Hello, do you like tea? <|endoftext|> In the sunlit terraces"
      	"of someunknownPlace."
 		)
 
+print("Original text follows:")
+print(text)
+
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print("Encoded values:")
 print(integers)
 
 strings = tokenizer.decode(integers)
+print("Decoded text:")
 print(strings)
 
+print("Test value", tokenizer.decode([27271]))
